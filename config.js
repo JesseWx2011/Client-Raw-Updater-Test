3b31d922d3122d043c129f836c3de50601ea499f
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2025-09-09 11:05:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 //Ajust these values to your liking.
 var clientRawName = "https://raw.githubusercontent.com/JesseWx2011/ClientRaw-Updater/refs/heads/master/clientraw.txt", //The names of your clientraw files
-    clientRawExtraName = "clientrawextra.txt",
-    clientRawHourName = "clientrawhour.txt",
-    clientRawDailyName = "clientrawdaily.txt",
-    customBaseURL = false, // OPTIONAL: Set the path to where your clientraw files are uploaded e.g., "http://www.goldenbaynzweather.info/wdl/" (note: final backslash and quotation marks must be included). Otherwise leave as: false
+    clientRawExtraName = "https://raw.githubusercontent.com/JesseWx2011/ClientRaw-Updater/refs/heads/master/clientrawextra.txt",
+    clientRawHourName = "https://raw.githubusercontent.com/JesseWx2011/ClientRaw-Updater/refs/heads/master/clientrawhour.txt",
+    clientRawDailyName = "https://raw.githubusercontent.com/JesseWx2011/ClientRaw-Updater/refs/heads/master/clientrawdaily.txt",
+    customBaseURL = true, // OPTIONAL: Set the path to where your clientraw files are uploaded e.g., "http://www.goldenbaynzweather.info/wdl/" (note: final backslash and quotation marks must be included). Otherwise leave as: false
     lang = "en", //Set Language. To see what lanuages are currently supported, see the readme file at: https://github.com/Yerren/FreshWDL/blob/master/README.md
     generalSettings = {
         tooltipsEnabled: true  //Disable or enable tooltips.
